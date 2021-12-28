da156fd644e2e6fa4fe92550cc53569458507f44
Nerjal Nosk <nosk.nerjal@gmx.com>
2021-12-28 18:34:27 +0100

    It doesn't work, but more tests

--- src/com/nerjal/json/Tests.java
+++ src/com/nerjal/json/Tests.java
@@ -3,5 +3,10 @@ package com.nerjal.json;
 public abstract class Tests {
     public static void main(String[] args) throws JsonError.JsonParseException, JsonError.JsonElementTypeException {
         System.out.println(JsonParser.parseJson(JsonParser.parseString("{a : 1}")));
+        System.out.println(JsonParser.parseJson(JsonParser.parseString("['a',1]")));
+        System.out.println(JsonParser.parseJson(JsonParser.parseString("{'a':1}")));
+        System.out.println(JsonParser.parseJson(JsonParser.parseString("{a:'1'}")));
+        System.out.println(JsonParser.parseJson(JsonParser.parseString("{a:\"1\\\n\"}")));
+        System.out.println(JsonParser.parseJson(JsonParser.parseString("{a : 1/**/}")));
     }
 }
