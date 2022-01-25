26f89127d438b15c3f81db6a0612f828de6568f2
Nerjal Nosk <nosk.nerjal@gmx.com>
2022-01-25 14:48:37 +0100

    Added JsonStringifyStack documentation

--- src/com/nerjal/json/elements/JsonStringifyStack.java
+++ src/com/nerjal/json/elements/JsonStringifyStack.java
@@ -3,13 +3,37 @@ package com.nerjal.json.elements;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * <p>An object used to store the
+ * stringification stack of a JSON
+ * structure, in order to avoid
+ * parsing a circular structure.
+ * </p>
+ * Stack works on the elements'
+ * hash.
+ */
 class JsonStringifyStack {
     private final List<Integer> stack = new ArrayList<>();
 
+    /**
+     * Instantiates a new stack with the
+     * specified element's hash.
+     * @param e the first element to add
+     *          to the stack.
+     */
     public JsonStringifyStack(JsonElement e) {
         this.stack.add(e.hashCode());
     }
 
+    /**
+     * Either returns if the stack already
+     * has the specified element's hash
+     * in it, or adds it.
+     * @param i the element's hash to search
+     *          for in the stack
+     * @return whether the given hash was
+     *         already in the stack or not.
+     */
     private int findOrAdd(int i) {
         int l = 0;
         int h = stack.size();
@@ -34,15 +58,25 @@ class JsonStringifyStack {
     }
 
     /**
-     * Either tells if a {@link JsonElement} is already in the stack, or
-     * adds it.
-     * @return true if the element is already in the stack
-     * (error case), false otherwise.
+     * Either tells if the specified
+     * JsonElement is already in the stack,
+     * or adds it.
+     * @return true if the element is already
+     *         in the stack (error case),
+     *         false otherwise.
      */
     public final boolean hasOrAdd(JsonElement e) {
         return findOrAdd(e.hashCode()) < 0;
     }
 
+    /**
+     * Removes the specified element's
+     * hash from the stack.<br>
+     * Used when closing an element while
+     * parsing it to string.
+     * @param e the element to remove
+     *          from the stack
+     */
     public final void rem(JsonElement e) {
         int i = e.hashCode();
         int up = stack.size();
