895d90610c58d10f31ebbab644b2a530ab6b15e3
Nerjal Nosk <nosk.nerjal@gmx.com>
2022-01-25 13:55:25 +0100

    Added JsonComment documentation

--- src/com/nerjal/json/elements/JsonComment.java
+++ src/com/nerjal/json/elements/JsonComment.java
@@ -3,27 +3,21 @@ package com.nerjal.json.elements;
 
 import com.nerjal.json.parser.options.CommentParseOptions;
 
-import java.util.Collection;
+import java.util.function.Consumer;
 
 /**
- * The {@code JsonComment} class represents a JSON comment.<br>
- * Those are structured the same way as Java or JS comments.
- * <p><br>
- * Single line comments start with // and continue to the end
- * of the line.<br>
- * Block comments are opened with the {@code /*} characters
- * sequence, and closed whenever comes the exact opposite
- * sequence.
- * <p><br>
- * Those are contained in container nodes ( {@link JsonObject}
- * and {@link JsonArray} ) but are not included in iterations
- * such as {@code jsonArray.forEach(e -> {})} or
- * <blockquote><pre>
- *     for (JsonElement element : jsonArray) {
- *         ...
- *     }
- * </pre></blockquote>
- * Unless using {@code jsonArray.forAll(e -> {})}
+ * <p>Represents a JSON comment, inside a
+ * JSON structure. Thus allowing to keep
+ * them inside a JSON file despite editing
+ * it.
+ * </p>
+ * For functional reasons, they are ignored
+ * while iterating through composite
+ * elements with basic methods. Although
+ * some methods, such as
+ * {@link JsonArray#forAll}, allow to still
+ * get them, and therefore edit them if
+ * wished to.
  */
 public class JsonComment extends JsonElement {
     private String value;
@@ -31,6 +25,50 @@ public class JsonComment extends JsonElement {
     private boolean lockBlock = false;
     private final CommentParseOptions parseOptions = new CommentParseOptions();
 
+    /**
+     * Instantiates a new in-line JsonComment
+     * with a {@code null} value.
+     */
+    public JsonComment() {
+        this(null, false);
+    }
+
+    /**
+     * Instantiates a new JsonComment with
+     * the specified value.<br>
+     * By default, the comment is inline,
+     * unless the specified value has more
+     * than one line ({@code s.split('\n')})
+     * @param s the new comment's value
+     */
+    public JsonComment(String s) {
+        this(s, false);
+    }
+
+    /**
+     * Instantiates a new JsonComment with
+     * a null value.<br>
+     * Whether the new comment is block or
+     * inline is set by the specified boolean.
+     * @param b whether the comment shall be
+     *          in-line or not.
+     */
+    public JsonComment(boolean b) {
+        this(null, b);
+    }
+
+    /**
+     * Sets a new JsonComment with the
+     * specified value and specified 'block'
+     * property.
+     * @param s the new comment's value
+     * @param b whether the comment shall
+     *          be in-line or not
+     * @throws UnsupportedOperationException if
+     *         the new comment is specified to
+     *         be in-line but the value is
+     *         multi-line.
+     */
     public JsonComment(String s, boolean b) {
         this.value = s;
         if (s != null && s.split("\n").length > 1)
@@ -40,18 +78,13 @@ public class JsonComment extends JsonElement {
         this.isBlock = b;
     }
 
-    public JsonComment(String s) {
-        this(s, false);
-    }
-
-    public JsonComment(boolean b) {
-        this(null, b);
-    }
-
-    public JsonComment() {
-        this(null, false);
-    }
-
+    /**
+     * Sets the comment value to the specified
+     * string.<br>
+     * If the specified string is multi-line,
+     * the comment is set to block.
+     * @param s the comment's new value
+     */
     public void setValue(String s) {
         this.value = s;
         if (s != null && s.split("\n").length > 1) {
@@ -60,20 +93,48 @@ public class JsonComment extends JsonElement {
         } else this.lockBlock = false;
     }
 
+    /**
+     * Sets whether the comment shall be block
+     * or not (in-line).
+     * @param block whether the comment shall
+     *              be block or not
+     * @throws UnsupportedOperationException if
+     *         the new comment is specified to
+     *         be in-line but the value is
+     *         multi-line.
+     */
     public void setBlock(boolean block) throws UnsupportedOperationException {
         if (!block && this.lockBlock)
             throw new UnsupportedOperationException("Multi-line comment cannot be set as non-block");
         isBlock = block;
     }
 
+    /**
+     * Returns whether the comment is a block
+     * comment.
+     * @return whether the comment is a block
+     *         comment
+     */
     public boolean isBlock() {
         return isBlock;
     }
 
+    /**
+     * Returns the comment's stringification
+     * options.
+     * @return the comment's stringification
+     *         options
+     */
     public CommentParseOptions getParseOptions() {
         return parseOptions;
     }
 
+    /**
+     * Returns he comment's value, split by
+     * line.
+     * @return the comment's value, split by
+     *         line
+     */
     public String[] getSplitValue() {
         return this.value.split("\n");
     }
