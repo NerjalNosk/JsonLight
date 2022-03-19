37cceee4039f710f773b65e7522564eb126b9000
Nerjal Nosk <nosk.nerjal@gmx.com>
2022-03-20 00:46:07 +0100

    Added package documentation (com/nerjal/json/package-info.java) + build

---
+++ src/main/java/com/nerjal/json/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package provides the ability to instantiate and manipulate JSON
+ * structures within Java.
+ *
+ * Parsing back and forth with strings is also fully supported using the
+ * {@link com.nerjal.json.JsonParser} class's methods.
+ *
+ * @author Nerjal Nosk
+ * @version 1.1.3
+ * @see <a href="https://github.com/NerjalNosk/JsonLight">official repo</a>
+ */
+package com.nerjal.json;
